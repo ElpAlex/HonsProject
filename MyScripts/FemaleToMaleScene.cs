471c174ed249c7ebe544973a78ee7a66484ea5f0
ElpAlex <52038964+ElpAlex@users.noreply.github.com>
2021-08-30 19:17:41 +0100

    Update FemaleToMaleScene.cs

--- MyScripts/FemaleToMaleScene.cs
+++ MyScripts/FemaleToMaleScene.cs
@@ -2,32 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//changing the scene from the one of the customization of the female model to the one of the male model
 public class FemaleToMaleScene : MonoBehaviour
 {
-    //public GameObject MainFemaleScene;
-
     public GameObject FemaleModel;
     public GameObject MaleModel;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        ///FemaleModel.SetActive(false);
-        //MaleObject.SetActive(false);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //if(MainFemaleScene.activeInHierarchy)
-        //{
-           // FemaleModel.SetActive(true);
-           // MaleObject.SetActive(false);
-       // }
-        //else
-       // {
+        
             FemaleModel.SetActive(false);
             MaleModel.SetActive(true);
-       // }
+      
     }
 }
